2626c582d440b3691dd5c95cd7b7d56ffa0d65cc
tesha <benedict.tesha@jamaatech.com>
2018-04-28 12:25:58 +0300

    Change message

--- menus/vendors/register.js
+++ menus/vendors/register.js
@@ -1,6 +1,6 @@
 
 module.exports = {
     main(){
-        this.send('Ups! This menu has not been implented yet!', true);
+        this.send('Sorry! You cannot register distributors at the moment!', true);
     }
 }
