ad8868c203602ee951a7f06adbcb7907756e3b50
tesha <benedict.tesha@jamaatech.com>
2018-06-21 09:20:25 +0300

    Display login user on UI

--- routes/protected.js
+++ routes/protected.js
@@ -2,6 +2,16 @@ const { urls } = require("../config");
 
 module.exports = function(request, response, next) {
   if (request.user) {
+    //Replace response.render method so we can pass
+    //session info to the template
+    const originalRenderFunc = response.render;
+    response.render = function(template, context){
+      context = context || {};
+      context.session = {
+        user: request.user
+      };
+      originalRenderFunc.call(this, ...arguments)
+    }
     next();
   } else {
     response.redirect(urls.login);
